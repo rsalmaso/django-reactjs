3af7b6098405546936df0e3b4e6904e348265b71
Raffaele Salmaso <raffaele@salmaso.org>
2016-09-12 18:32:05 +0200

    renamed Config to ReactJSConfig

--- reactjs/apps.py
+++ reactjs/apps.py
@@ -25,6 +25,6 @@ from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
 
-class Config(AppConfig):
+class ReactJSConfig(AppConfig):
     name = "reactjs"
     verbose_name = _("ReactJS")
